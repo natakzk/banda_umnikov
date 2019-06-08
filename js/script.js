952a42e47fa9668f3d159d75c51339c1370a3210
Natasha Kazakova <mewse@yandex.ru>
2019-06-08 10:21:47 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -38,6 +38,6 @@ function initializeClock(id, endtime) {
   var timeinterval = setInterval(updateClock, 1000);
 }
 
-var deadline="June 08 2019 00:00:00 GMT+0300";
+var deadline="June 13 2019 00:00:00 GMT+0300";
 initializeClock('clockdiv', deadline);
 
